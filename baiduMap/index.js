ea41a88ef70c60b4641253649f59a93659cc0c82
jim <lvjiangmin@niiwoo.com>
2019-03-18 11:14:51 +0800

    '添加了域名识别'

--- baiduMap/index.js
+++ baiduMap/index.js
@@ -15,94 +15,95 @@
 // 待选   willSelect: background: #00e0cb;
 
 
-
-window.onload = ()=>{ 
-    setTimeout(()=>{  
-        var allTypes = ['passed','hasNotLook','willLook','looked','oneStar','twoStar','threeStar','willSelect'];
-        // 小区分类目录
-        var allTypeObj = {
-            passed: [],
-            hasNotLook: [],
-            willLook: [],
-            looked: ["银座金钻", "国际名园", "庐山花园","凯旋TRC", "汇泰大厦", "怡都大厦", "大中华汇展阁", "港岛银座", "星桂园-C座", "名仕阁", "好年华东门金座", "立新花园(人民北路)", "碧海富通城-三期", "碧海富通城-四期", "广兴源圣拿威湾美花园", "祥福雅居", "庐江春天公寓", "荔馨村南山区", "桃苑公寓", "澎柏·白金假日公寓", "大都汇大厦", "龙珠花园", "国际市长交流中心", "友谊大厦", "南洋大厦", "东海大厦", "宏丰大厦罗湖"],
-            oneStar: [],
-            twoStar: [],
-            threeStar: [],
-            willSelect: [],            
-        }
-        // style: 生成样式
-        var styleTag = document.createElement('style');
-        styleTag.innerHTML = `
-            .jimMark {
-                width: 50px;
-                background: #607D8B;
-                color: #fff;
-                padding: 2px;
-                font-family: microsoft yahei;
-                line-height: 1.2;
+if(location.hostname.includes('map.baidu.com')){  
+    window.onload = ()=>{ 
+        setTimeout(()=>{  
+            var allTypes = ['passed','hasNotLook','willLook','looked','oneStar','twoStar','threeStar','willSelect'];
+            // 小区分类目录
+            var allTypeObj = {
+                passed: [],
+                hasNotLook: [],
+                willLook: [],
+                looked: ["银座金钻", "国际名园", "庐山花园","凯旋TRC", "汇泰大厦", "怡都大厦", "大中华汇展阁", "港岛银座", "星桂园-C座", "名仕阁", "好年华东门金座", "立新花园(人民北路)", "碧海富通城-三期", "碧海富通城-四期", "广兴源圣拿威湾美花园", "祥福雅居", "庐江春天公寓", "荔馨村南山区", "桃苑公寓", "澎柏·白金假日公寓", "大都汇大厦", "龙珠花园", "国际市长交流中心", "友谊大厦", "南洋大厦", "东海大厦", "宏丰大厦罗湖"],
+                oneStar: [],
+                twoStar: [],
+                threeStar: [],
+                willSelect: [],            
             }
-            .jimMark.passed {
-                background: #9E9E9E;   
-            }
-            .jimMark.hasNotLook {
-                background: #607D8B;   
-            }
-            .jimMark.willLook {
-                background: #FF9800;   
-            }
-            .jimMark.looked {
-                background: #3385ff;   
-            }
-            .jimMark.oneStar {
-                background: #8BC34A;   
-            }
-            .jimMark.twoStar {
-                background: #4CAF50;   
-            }
-            .jimMark.threeStar {
-                background: #009688;   
-            }
-            .jimMark.willSelect {
-                background: #00e0cb;   
-            }
-        `
-        document.head.appendChild(styleTag)
-        console.log('百度地图收藏夹: 样式添加成功');  
-
-        // 所有收藏夹
-        var mark = document.getElementsByClassName('BMap_Marker')
-        // 标注所有收藏夹
-        Array.from(mark).map(item=>{
-            item.innerHTML = 
-            `<div class="jimMark">${item.title}</div>`
-        })
-        // 标注已看小区
-        var jimMark = document.getElementsByClassName('jimMark');
-        Array.from(jimMark).map(item=>{
-            allTypes.map(type=>{
-                if(allTypeObj[type].includes(item.innerText)){
-                    item.className = item.className + ' ' + type;
+            // style: 生成样式
+            var styleTag = document.createElement('style');
+            styleTag.innerHTML = `
+                .jimMark {
+                    width: 50px;
+                    background: #607D8B;
+                    color: #fff;
+                    padding: 2px;
+                    font-family: microsoft yahei;
+                    line-height: 1.2;
                 }
-            })
-        })
-        // 打印汇总
-        var allMarkedObj = {
-            passed: { name : "筛掉", sum : 0, marked: 0 },
-            hasNotLook: { name : "未看", sum : 0, marked: 0 },
-            willLook: { name : "计划", sum : 0, marked: 0 },
-            looked: { name : "已看", sum : 0, marked: 0 },
-            oneStar: { name : "一星", sum : 0, marked: 0 },
-            twoStar: { name : "二星", sum : 0, marked: 0 },
-            threeStar: { name : "三星", sum : 0, marked: 0 },
-            willSelect: { name : "待选", sum : 0, marked: 0 },
-        }
-        allTypes.map(item=>{
-            allMarkedObj[item].sum = allTypeObj[item].length;
-            allMarkedObj[item].marked = Array.from(document.getElementsByClassName('jimMark '+item)).length;
-        })
+                .jimMark.passed {
+                    background: #9E9E9E;   
+                }
+                .jimMark.hasNotLook {
+                    background: #607D8B;   
+                }
+                .jimMark.willLook {
+                    background: #FF9800;   
+                }
+                .jimMark.looked {
+                    background: #3385ff;   
+                }
+                .jimMark.oneStar {
+                    background: #8BC34A;   
+                }
+                .jimMark.twoStar {
+                    background: #4CAF50;   
+                }
+                .jimMark.threeStar {
+                    background: #009688;   
+                }
+                .jimMark.willSelect {
+                    background: #00e0cb;   
+                }
+            `
+            document.head.appendChild(styleTag)
+            console.log('百度地图收藏夹: 样式添加成功');  
 
-        console.table(Object.values(allMarkedObj), ["name","sum","marked"]);
-        console.log('百度地图收藏夹: 脚本执行完毕');    
-    },3000)
+            // 所有收藏夹
+            var mark = document.getElementsByClassName('BMap_Marker')
+            // 标注所有收藏夹
+            Array.from(mark).map(item=>{
+                item.innerHTML = 
+                `<div class="jimMark">${item.title}</div>`
+            })
+            // 标注已看小区
+            var jimMark = document.getElementsByClassName('jimMark');
+            Array.from(jimMark).map(item=>{
+                allTypes.map(type=>{
+                    if(allTypeObj[type].includes(item.innerText)){
+                        item.className = item.className + ' ' + type;
+                    }
+                })
+            })
+            // 打印汇总
+            var allMarkedObj = {
+                passed: { name : "筛掉", sum : 0, marked: 0 },
+                hasNotLook: { name : "未看", sum : 0, marked: 0 },
+                willLook: { name : "计划", sum : 0, marked: 0 },
+                looked: { name : "已看", sum : 0, marked: 0 },
+                oneStar: { name : "一星", sum : 0, marked: 0 },
+                twoStar: { name : "二星", sum : 0, marked: 0 },
+                threeStar: { name : "三星", sum : 0, marked: 0 },
+                willSelect: { name : "待选", sum : 0, marked: 0 },
+            }
+            allTypes.map(item=>{
+                allMarkedObj[item].sum = allTypeObj[item].length;
+                allMarkedObj[item].marked = Array.from(document.getElementsByClassName('jimMark '+item)).length;
+            })
+
+            console.table(Object.values(allMarkedObj), ["name","sum","marked"]);
+            console.log('百度地图收藏夹: 脚本执行完毕');    
+        },3000)
+    }
 }
 
--- lianjia/index.js
+++ lianjia/index.js
@@ -4,11 +4,14 @@
 // @match *://*/*
 // @grant none
 // ==/UserScript==
-// 文本可选
-document.querySelectorAll('span').forEach(item=>item.style = '-webkit-user-select:text;');
-document.querySelectorAll('li').forEach(item=>item.style = '-webkit-user-select:text;')
-document.body.style = '-webkit-user-select:text;'
-// PC隐藏二维码
-document.getElementsByClassName('layer-qrcode')[0].style.display = 'none'
-// 看房记录:已看小区
-var looked = Array.from(new Set(Array.from(document.getElementsByClassName('region')).map(item=>item.innerText)));
+
+if(location.hostname.includes('lianjia.com')){ 
+    // 文本可选
+    document.querySelectorAll('span').forEach(item=>item.style = '-webkit-user-select:text;');
+    document.querySelectorAll('li').forEach(item=>item.style = '-webkit-user-select:text;')
+    document.body.style = '-webkit-user-select:text;'
+    // PC隐藏二维码
+    document.getElementsByClassName('layer-qrcode')[0].style.display = 'none'
+    // 看房记录:已看小区
+    var looked = Array.from(new Set(Array.from(document.getElementsByClassName('region')).map(item=>item.innerText)));
+}
